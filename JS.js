ad6d21551a4fde3ed5f502c34599c5e26888d6d4
JRichtar <JRichtar@users.noreply.github.com>
2021-11-11 17:12:43 +0100

    .

---
+++ JS.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+import { Viewer } from 'photo-sphere-viewer';
+
+const viewer = new Viewer({
+  container: document.querySelector('#viewer'),
+  panorama: 'path/to/panorama.jpg'
+});
