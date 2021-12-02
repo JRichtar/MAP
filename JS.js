02418f5ecb65334997cf4c20f23b6f23bab54f58
DanielMares-Bambula <79077162+DanielMares-Bambula@users.noreply.github.com>
2021-12-02 20:18:25 +0100

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,10 +1,15 @@
 // JavaScript Document
 
 import { Viewer } from 'photo-sphere-viewer';
-
-const viewer = new Viewer({
-  container: document.querySelector('#viewer'),
-  panorama: 'path/to/panorama.jpg'
-});
-
-
+var app = {
+	const viewer = new Viewer({
+             container: document.querySelector('#viewer'),
+             panorama: 'path/to/panorama.jpg',
+             });
+	fetch("test.json").then((response) => {
+			return response.json();
+		}).then((json) => {
+			app.rooms = json;
+		})
+}
+const viewers = JSON.parse(text);
