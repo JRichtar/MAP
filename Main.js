9bf695c8b1fadf4cb8c3d1359658f3ed32535ac9
JRichtár <jakub.richtar02@gmail.com>
2022-01-12 21:19:49 +0100

    new 12.1.22

--- Main.js
+++ Main.js
@@ -10,8 +10,8 @@ var app = {
 			plugins: [
 				PhotoSphereViewer.MarkersPlugin,
 				[PhotoSphereViewer.VirtualTourPlugin, {
-					positionMode: PhotoSphereViewer.VirtualTourPlugin.MODE_GPS,
-					renderMode: PhotoSphereViewer.VirtualTourPlugin.MODE_3D,
+					positionMode: PhotoSphereViewer.VirtualTourPlugin.MODE_MANUAL,
+					renderMode: PhotoSphereViewer.VirtualTourPlugin.MODE_MARKERS,
 				}],
 			],
 		});
@@ -20,12 +20,22 @@ var app = {
 		app.loadNodes();
 	},
 	loadNodes: function() {
-		fetch("nodes.json").then((response) => {
-			return response.json();
-		}).then((json) => {
-			app.nodes = json;
-			app.tour.setNodes(app.nodes);
-		});
-	}
+        fetch("nodes.json").then((response) => {
+            return response.json();
+        }).then((json) => {
+            for (let i = 0; i<json.length; i++) {
+                for (let j = 0; j<json[i].links.length; j++) {
+                    json[i].links[j].latitude = json[i].links[j].latitude*(Math.PI/180);
+                    json[i].links[j].longitude = json[i].links[j].longitude*(Math.PI/180);
+                }
+                for (let j = 0; j<json[i].markers.length; j++) {
+                    json[i].markers[j].latitude = json[i].markers[j].latitude*(Math.PI/180);
+                    json[i].markers[j].longitude = json[i].markers[j].longitude*(Math.PI/180);
+                }
+            }
+            app.nodes = json;
+            app.tour.setNodes(app.nodes);
+        });
+    }
 }
 window.addEventListener("load", app.init);
