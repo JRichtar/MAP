11fc604cf0bc885b869021ed370ff386e9a3121e
Aksuoha <houska.ton@gmail.com>
2022-12-07 11:46:07 +0100

    Add pouzeOddeleni 7.12.22
    složka "pouze oddeleni", kde jsou pouze oddělení, každé ve vlastní složce s vlastním mainem. html a css soubor je pro všechny úplně stejný, tak snad stačí jeden

---
+++ pouze oddeleni/parkPredNemocnici/Main.js
@@ -0,0 +1,46 @@
+var app = {
+	nodes: null,
+	viewer: null,
+	tour: null,
+
+	init: function() {
+		app.viewer = new PhotoSphereViewer.Viewer({
+			container: document.querySelector('#viewer'),
+			panorama: 'ParkPredNemocnici/001.jpg',
+			plugins: [
+				PhotoSphereViewer.MarkersPlugin,
+				[PhotoSphereViewer.VirtualTourPlugin, {
+					positionMode: PhotoSphereViewer.VirtualTourPlugin.MODE_MANUAL,
+					renderMode: PhotoSphereViewer.VirtualTourPlugin.MODE_MARKERS,
+				}],
+			],
+		});
+		app.tour = app.viewer.getPlugin(PhotoSphereViewer.VirtualTourPlugin);
+		app.loadNodes();
+	},
+	loadNodes: function() {
+        fetch("parkPredNemocnici.json").then((response) => {
+            return response.json();
+        }).then((json) => {
+            for (let i = 0; i<json.length; i++) {
+                for (let j = 0; j<json[i].links.length; j++) {
+                    json[i].links[j].latitude = json[i].links[j].latitude*(Math.PI/180);
+                    json[i].links[j].longitude = json[i].links[j].longitude*(Math.PI/180);
+                }
+               if (json[i].markers) {
+				    for (let j = 0; j<json[i].markers.length; j++) {
+				        json[i].markers[j].latitude = json[i].markers[j].latitude*(Math.PI/180);
+				        json[i].markers[j].longitude = json[i].markers[j].longitude*(Math.PI/180);
+
+						if (json[i].markers[j].content_id) {
+		                    json[i].markers[j].content = document.getElementById(json[i].markers[j].content_id).innerHTML;
+						}
+				    }
+				}
+            }
+            app.nodes = json;
+            app.tour.setNodes(app.nodes);
+        });
+    }
+}
+window.addEventListener("load", app.init);
